ec277653ad03e88f2ab00bd909f05f8777e21fc8
Administrator <52603245@qq.com>
2020-04-23 12:12:21 +0800

    完成了子弹发射

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 import game_functions as gf
 from settings import Settings
 from ship import Ship
+from pygame.sprite import Group
 
 def run_game():
     # 初始化游戏，并创建一个屏幕对象
@@ -12,18 +13,18 @@ def run_game():
 
     # 创建一艘飞船
     ship=Ship(ai_settings,screen)
-
-    # 设置背景颜色
-    bg_color=(ai_settings.bg_color)
+    # 创建一个用于存储子弹的编组
+    bullets=Group()
 
     # 开始游戏的主循环
     while True:
 
         # 监视键盘和鼠标事件
-        gf.check_events(ship)
+        gf.check_events(ai_settings,screen,ship,bullets)
 
         ship.update()
+        gf.update_bullets(bullets)
         # 每次循环时都重绘屏幕
-        gf.update_screen(ai_settings,screen,ship)
+        gf.update_screen(ai_settings,screen,ship,bullets)
 
 run_game()
